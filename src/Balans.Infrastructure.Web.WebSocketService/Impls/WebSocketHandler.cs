7b2966b8b84856ad9f2a2ff35775877b52c367b5
Ghis2Zeus <ghislainzeleu@protonmail.com>
2019-04-03 14:14:21 +0200

    Middlware has been implemented

---
+++ src/Balans.Infrastructure.Web.WebSocketService/Impls/WebSocketHandler.cs
@@ -0,0 +1,60 @@
+﻿namespace Balans.Infrastructure.Web.WebSocketService.Impls
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Net.WebSockets;
+  using System.Text;
+  using System.Threading;
+  using System.Threading.Tasks;
+
+  public abstract class WebSocketHandler
+  {
+    protected IWebSocketConnectionManager WebSocketConnectionManager { get; set; }
+
+    public WebSocketHandler(IWebSocketConnectionManager webSocketConnectionManager)
+    {
+      WebSocketConnectionManager = webSocketConnectionManager;
+    }
+
+    public virtual async Task OnConnected(WebSocket socket)
+    {
+      WebSocketConnectionManager.AddSocket(socket);
+    }
+
+    public virtual async Task OnDisconnected(WebSocket socket)
+    {
+      await WebSocketConnectionManager.RemoveSocketAsync(WebSocketConnectionManager.GetId(socket));
+    }
+
+    public async Task SendMessageAsync(WebSocket socket, string message)
+    {
+      if (socket.State != WebSocketState.Open)
+      {
+        return;
+      }
+
+      await socket.SendAsync(buffer: new ArraySegment<byte>(array: Encoding.ASCII.GetBytes(message),
+                                                            offset: 0,
+                                                            count: message.Length),
+                             messageType: WebSocketMessageType.Text,
+                             endOfMessage: true,
+                             cancellationToken: CancellationToken.None);
+    }
+
+    public async Task SendMessageAsync(string socketId, string message)
+    {
+      await SendMessageAsync(WebSocketConnectionManager.GetSocketById(socketId), message);
+    }
+
+    public async Task SendMessageToAllAsync(string message)
+    {
+      foreach (var pair in WebSocketConnectionManager.GetAll())
+      {
+        if (pair.Value.State == WebSocketState.Open)
+          await SendMessageAsync(pair.Value, message);
+      }
+    }
+
+    public abstract Task ReceiveAsync(WebSocket socket, WebSocketReceiveResult result, byte[] buffer);
+  }
+}
