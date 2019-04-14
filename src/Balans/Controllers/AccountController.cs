ca6c47b48013a6f47d5b1805d78add87ef34380f
wmorian <wmorian@pm.me>
2019-04-14 21:32:01 +0200

    revert AccountController

--- src/Balans/Controllers/AccountController.cs
+++ src/Balans/Controllers/AccountController.cs
@@ -12,8 +12,8 @@ namespace Balans.Controllers
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
     {
-        private readonly IConfiguration configuration;
         private readonly AccountContext context;
+        private readonly IConfiguration configuration;
         private readonly string path;
 
         public AccountController(AccountContext context, IConfiguration configuration)
@@ -21,59 +21,6 @@ namespace Balans.Controllers
             this.context = context;
             this.configuration = configuration;
             this.path = this.configuration.GetSection("dbPath").Value;
-        }
-
-        [HttpPost("addentity/{username}")]
-        public IActionResult AddEntity(string username, [FromBody] EntityDto entityDto)
-        {
-            if (!this.ModelState.IsValid)
-            {
-                return this.BadRequest(this.ModelState);
-            }
-
-            var account = this.context.Accounts.FirstOrDefault(a => a.Username == username);
-
-            if (account == null)
-            {
-<<<<<<< HEAD
-                return this.NotFound(user);
-            }
-
-            var entityDtos = account.Entities.Select(e => new EntityDto { Name = e.Name, Amount = e.Amount }).ToList();
-=======
-                return this.NotFound("User not found.");
-            }
->>>>>>> fe22c819016e116a8f1e819d73728bc8a604ccd6
-
-            var entity = new Entity { Name = entityDto.Name, Amount = entityDto.Amount };
-
-            account.Entities.Add(entity);
-
-            this.context.SaveChanges();
-
-            return this.Ok();
-        }
-
-        [HttpPost("create")]
-        public IActionResult Create([FromBody] AccountDto accountDto)
-        {
-            if (!this.ModelState.IsValid)
-            {
-                return this.BadRequest(this.ModelState);
-            }
-
-            //this.SaveAccount(account);
-
-            if (this.context.Accounts.Any(a => a.Username == accountDto.Username))
-            {
-                return this.Forbid($"{accountDto.Username} already exists.");
-            }
-
-            var account = new Account { Username = accountDto.Username, /*Entities = accountDto.Entities*/ };
-            this.context.Accounts.Add(account);
-            this.context.SaveChanges();
-
-            return this.Ok();
         }
 
         [HttpGet("{user}")]
@@ -86,50 +33,16 @@ namespace Balans.Controllers
 
             var account = this.context.Accounts.Include(a => a.Entities).FirstOrDefault(a => a.Username == user);
 
+            if (account == null)
+            {
+                return this.NotFound(user);
+            }
+
             var entityDtos = account.Entities.Select(e => new EntityDto { Name = e.Name, Amount = e.Amount }).ToList();
 
             var accountDto = new AccountDto { Username = account.Username, Entities = entityDtos };
 
             return this.Ok(accountDto);
-        }
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var accountDto = this.context.Accounts;
-            return this.Ok(accountDto);
-        }
-
-        [HttpGet("balance/{username}")]
-        public IActionResult GetBalance(string username)
-        {
-            if (!this.ModelState.IsValid)
-            {
-                return this.BadRequest(this.ModelState);
-            }
-
-<<<<<<< HEAD
-            if (this.context.Accounts.Any(a => a.Username == accountDto.Username))
-=======
-            var account = this.context.Accounts.Include(a => a.Entities).FirstOrDefault(a => a.Username == username);
-
-            if (account == null)
->>>>>>> fe22c819016e116a8f1e819d73728bc8a604ccd6
-            {
-                return this.NotFound("User not found.");
-            }
-
-<<<<<<< HEAD
-            var account = new Account { Username = accountDto.Username, CreateTime = DateTime.Now };
-            this.context.Accounts.Add(account);
-            this.context.SaveChanges();
-
-            return this.Ok();
-=======
-            float balance = account.Entities.Sum(e => e.Amount);
-
-            return this.Ok(balance);
->>>>>>> fe22c819016e116a8f1e819d73728bc8a604ccd6
         }
 
         [HttpGet("entities/{user}")]
@@ -147,17 +60,73 @@ namespace Balans.Controllers
                 return this.NotFound($"Account with the Id: {account} not found.");
             }
 
-<<<<<<< HEAD
+            var result = account.Entities.Select(e => new EntityDto { Name = e.Name, Amount = e.Amount });
+
+            return this.Ok(result);
+        }
+
+        [HttpGet("balance/{username}")]
+        public IActionResult GetBalance(string username)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var account = this.context.Accounts.Include(a => a.Entities).FirstOrDefault(a => a.Username == username);
+
+            if (account == null)
+            {
+                return this.NotFound("User not found.");
+            }
+
+            float balance = account.Entities.Sum(e => e.Amount);
+
+            return this.Ok(balance);
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create([FromBody] AccountDto accountDto)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            if (this.context.Accounts.Any(a => a.Username == accountDto.Username))
+            {
+                return this.Forbid($"{accountDto.Username} already exists.");
+            }
+
+            var account = new Account { Username = accountDto.Username, CreateTime = DateTime.Now };
+            this.context.Accounts.Add(account);
+            this.context.SaveChanges();
+
+            return this.Ok();
+        }
+
+        [HttpPost("addentity/{username}")]
+        public IActionResult AddEntity(string username, [FromBody] EntityDto entityDto)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var account = this.context.Accounts.FirstOrDefault(a => a.Username == username);
+
+            if (account == null)
+            {
+                return this.NotFound("User not found.");
+            }
+
             var entity = new Entity { Name = entityDto.Name, Amount = entityDto.Amount, CreateTime = DateTime.Now };
 
             account.Entities.Add(entity);
 
             this.context.SaveChanges();
-=======
-            var result = account.Entities.Select(e => new EntityDto { Name = e.Name, Amount = e.Amount });
->>>>>>> fe22c819016e116a8f1e819d73728bc8a604ccd6
 
-            return this.Ok(result);
+            return this.Ok();
         }
     }
 }
